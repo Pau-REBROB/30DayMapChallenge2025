1adad7e617e58c55c8d9108449f2624591adf298
Pau Rebull Robert <137211058+Pau-REBROB@users.noreply.github.com>
2026-01-21 11:15:04 +0100

    Update Poblacio_Espanya_hexagons.R

--- Projectes/Poblacio_Espanya_hexagons/Script/Poblacio_Espanya_hexagons.R
+++ Projectes/Poblacio_Espanya_hexagons/Script/Poblacio_Espanya_hexagons.R
@@ -1,52 +1,79 @@
+# PAQUETS ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------- 
+
 library(readxl)
 library(sf)
 library(dplyr)
 library(stringr)
 library(ggplot2)
 
-# Importació de les dades de població
-dades_ine <- read_xlsx(path = "C:/30DayMapChallenge2025/Day25_Hexagons/pobmun/pobmun24.xlsx", skip = 1)
-# Creació codi municipi (província+municipi)
-dades_ine$CODI <- paste(dades_ine$CPRO, dades_ine$CMUN, sep = "")
 
-# Importació geometries municipis Espanya
+# ADQUISICIÓ DADES -------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+## Importació de les dades de població
+dades_ine <- read_xlsx(path = "C:/30DayMapChallenge2025/Day25_Hexagons/pobmun/pobmun24.xlsx",
+                       skip = 1)
+
+## Importació geometries municipis d'Espanya
 munis_esp <- st_read("C:/30DayMapChallenge2025/Day25_Hexagons/lineas_limite/SHP_ETRS89/recintos_municipales_inspire_peninbal_etrs89/recintos_municipales_inspire_peninbal_etrs89.shp")
-munis_esp <- st_transform(x = munis_esp, crs = 25830)
-# Creació del codi de municipi
-munis_esp$CODIMUN <- str_sub(string = munis_esp$NATCODE, start = -5, end = -1)
 
+## Importació geometries municipis d'Espanya, només el contorn 
 munis_linies <- st_read("C:/30DayMapChallenge2025/Day25_Hexagons/lineas_limite/SHP_ETRS89/ll_municipales_inspire_peninbal_etrs89/ll_municipales_inspire_peninbal_etrs89.shp")
-munis_linies <- st_transform(x = munis_linies, crs = 25830)
 
-# Join de les dades amb les geometries
-munis_esp_ine <- left_join(x = munis_esp, y = dades_ine, join_by(CODIMUN==CODI))
-
-# Dissolució en una geometria d'Espanya
-pol_esp <- st_union(munis_esp)
-
-
-# Importació de les províncies
+# Importació geometries de províncies
 prov_linies <- st_read("C:/30DayMapChallenge2025/Day25_Hexagons/lineas_limite/SHP_ETRS89/ll_provinciales_inspire_peninbal_etrs89/ll_provinciales_inspire_peninbal_etrs89.shp")
-prov_linies <- st_transform(x = prov_linies, crs = 25830)
 
 
+# TRANSFORMACIÓ DADES --------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-# ARA AMB HEXÀGONS MÉS PETITS
-# Creació de la malla
-malla150 <- st_make_grid(munis_esp,
-                         n = c(150,150),
-                         square = FALSE,
-                         crs = 25830)
-malla150 <- st_as_sf(malla150)
+## Creació codi municipi: codi província + codi municipi
+dades_ine$CODI <- paste(dades_ine$CPRO, 
+                        dades_ine$CMUN,
+                        sep = "")
 
-# Selecció dels hexàgons que intersecten amb el polígon d'espanya
-malla_retallada <- filter(malla150, lengths(st_intersects(malla150, pol_esp)) > 0)
+## Creació del codi de municipi a l'objecte de límits municipals, com a subcodi del que indica l'arxiu
+## per tal de coincidir amb el de l'INE
+munis_esp$CODIMUN <- str_sub(string = munis_esp$NATCODE, 
+                             start = -5,
+                             end = -1)
 
-# Unió dels atributs
-malla150_atributs <- st_join(malla_retallada, munis_esp_ine)
+## Transformació a SRC oficial
+munis_esp <- st_transform(x = munis_esp, 
+                          crs = 25830)
+munis_linies <- st_transform(x = munis_linies,
+                             crs = 25830)
+prov_linies <- st_transform(x = prov_linies, 
+                            crs = 25830)
+
+## Join de les dades de població amb les geometries 
+munis_esp_ine <- left_join(x = munis_esp,
+                           y = dades_ine,
+                           join_by(CODIMUN == CODI)) %>%
+
+## Creació d'un objecte amb la geometria d'Espanya
+pol_esp <- munis_esp %>%
+st_union() %>%
+st_as_sf()
 
 
-# Creació intervals de valors
+## Creació de la malla hexagonal
+malla150 <- munis_esp %>%
+st_make_grid(n = c(150,150),
+             square = FALSE,
+             crs = 25830) %>%
+st_as_sf()
+
+## Selecció dels hexàgons que intersecten amb el polígon d'espanya (no els retallem segons la seva geometria)
+malla_retallada <- filter(malla150,
+                          lengths(st_intersects(malla150, pol_esp)) > 0)
+
+## Unió dels atributs amb els hexàgons
+malla150_atributs <- st_join(malla_retallada,
+                             munis_esp_ine)
+
+
+# PLOTTING ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+## Creació intervals de valors de població
 malla150_atributs$intervals <- cut(
   malla150_atributs$POB24,
   breaks = c(0,1000,5000,20000,50000,100000,250000,500000,1000000,2000000,3000000, Inf),
@@ -56,7 +83,7 @@ malla150_atributs$intervals <- cut(
   include.lowest = TRUE
 )
 
-# Visualització
+## Visualització
 ggplot() + 
   geom_sf(data = malla150_atributs, aes(fill = intervals), color = 'white', linewidth = 0.2) +
   # geom_sf(data = prov_linies, linewidth = 0.05, alpha = 0.3) +
@@ -81,7 +108,7 @@ ggplot() +
            x = 900000, y = 3900000)
   
 
-# Exportar resultats
+## Exportació de resultats
 ggsave("mapa_poblacio.png", 
        width = 10, height = 8, units = "in",
        bg = 'white',
